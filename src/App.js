801f799f64b8d6b6bc9690e99a35ae5fdb78b9f9
Benriez <33022072+Benriez@users.noreply.github.com>
2020-06-23 12:18:51 +0200

    added basic timer features

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React, { Component } from 'react';
-import './App.css';
-import Timer from "./components/Timer";
+import React from 'react';
+import { useStopwatch } from 'react-timer-hook';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Stopwatch</h1>
-        <Timer />
+function MyStopwatch() {
+  const {
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    reset,
+  } = useStopwatch({ autoStart: true });
+
+
+  return (
+    <div style={{textAlign: 'center'}}>
+      <h1>React Stopwatch</h1>
+      <div style={{fontSize: '100px'}}>
+        <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
       </div>
-    );
-  }
-
+      <p>{isRunning ? 'Running' : 'Not running'}</p>
+      <button onClick={start}>Start</button>
+      <button onClick={pause}>Pause</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
 }
-export default App;
+
+export default function App() {
+  return (
+    <div>
+      <MyStopwatch />
+    </div>
+  );
+}
