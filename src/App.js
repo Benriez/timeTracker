9ebdcf3e5642c1d7463378383e6d9cd0b94bdbff
Benriez <33022072+Benriez@users.noreply.github.com>
2020-06-23 12:00:34 +0200

    startet new

--- src/App.js
+++ src/App.js
@@ -6,46 +6,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Stopwatch</h1>
-        <Stopwatch />
+        
       </div>
     );
   }
-}
 
-class Stopwatch extends Component {
-  state = {
-    status: false,
-    runningTime: 0
-  };
-  handleClick = () => {
-    this.setState(state => {
-      if (state.status) {
-        clearInterval(this.timer);
-      } else {
-        const startTime = Date.now() - this.state.runningTime;
-        this.timer = setInterval(() => {
-          this.setState({ runningTime: Date.now() - startTime });
-        });
-      }
-      console.log();
-      return { status: !state.status };
-    });
-  };
-  handleReset = () => {
-    clearInterval(this.timer); 
-    this.setState({ runningTime: 0, status: false });
-    console.log("check");
-  };
-  render() {
-    const { status, runningTime } = this.state;
-    return (
-      <div>
-        <p>{runningTime}ms</p>
-        <button onClick={this.handleClick}>{status ? 'Stop' : 'Start'}</button>
-        <button onClick={this.handleReset}>Reset</button>
-      </div>
-    );
-  }
 }
-
 export default App;
---
+++ src/components/Timer.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Timer extends Component {
+    render (){
+        const count = 1
+        return (
+            <div>
+                <h1>Current Count: {count}</h1>
+            </div>
+        )
+    }
+}
