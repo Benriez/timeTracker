643a9c25310b37a193a3a319cfb8bf783203a06d
Benriez <33022072+Benriez@users.noreply.github.com>
2020-06-23 11:23:43 +0200

    added react stopwatch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Clock from "./components/Clock";
-
+//import TimeTracking from "./components/TimeTracking";
 import './App.css';
 
 class App extends React.Component {
