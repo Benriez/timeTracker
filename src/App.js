352af3304082b90c94c2eaaafeafdbfb3f97ccdf
Benriez <33022072+Benriez@users.noreply.github.com>
2020-06-23 12:12:43 +0200

    created Timer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Timer from "./components/Timer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Stopwatch</h1>
-        
+        <Timer />
       </div>
     );
   }
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,12 +1,34 @@
 import React, {Component} from 'react';
 
 class Timer extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            count:0
+        }
+    }
+    
     render (){
-        const count = 1
+        const {count} = this.state
         return (
             <div>
                 <h1>Current Count: {count}</h1>
             </div>
         )
     }
+
+    //
+    componentDidMount (){
+        this.myInterval = setInterval(()=>{
+            this.setState(prevState => ({
+                count: prevState.count + 1
+            }))
+        }, 1000)
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.myInterval)
+    }
 }
+
+export default Timer
