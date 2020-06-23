f56960b64087d2a911e771381fb5bf0635e65e56
Benriez <33022072+Benriez@users.noreply.github.com>
2020-06-23 11:50:55 +0200

    created stopwatch component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-
-import Clock from "./components/Clock";
-//import TimeTracking from "./components/TimeTracking";
+import React, { Component } from 'react';
 import './App.css';
 
-class App extends React.Component {
-  
-  render () {
+class App extends Component {
+  render() {
     return (
-      <div className="container">
-        <h1>Time-Tracker-App</h1>
-        <Clock />
-        <p>0</p>
-        <button>Start</button>
-        <p>time tracked</p>
+      <div className="App">
+        <h1>Stopwatch</h1>
+        <Stopwatch />
       </div>
-      
-    )
+    );
   }
-
 }
 
+class Stopwatch extends Component {
+  state = {
+    status: false,
+    runningTime: 0
+  };
+  handleClick = () => {
+    this.setState(state => {
+      if (state.status) {
+        clearInterval(this.timer);
+      } else {
+        const startTime = Date.now() - this.state.runningTime;
+        this.timer = setInterval(() => {
+          this.setState({ runningTime: Date.now() - startTime });
+        });
+      }
+      console.log();
+      return { status: !state.status };
+    });
+  };
+  handleReset = () => {
+    clearInterval(this.timer); 
+    this.setState({ runningTime: 0, status: false });
+    console.log("check");
+  };
+  render() {
+    const { status, runningTime } = this.state;
+    return (
+      <div>
+        <p>{runningTime}ms</p>
+        <button onClick={this.handleClick}>{status ? 'Stop' : 'Start'}</button>
+        <button onClick={this.handleReset}>Reset</button>
+      </div>
+    );
+  }
+}
 
 export default App;
